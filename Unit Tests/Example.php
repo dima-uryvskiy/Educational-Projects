cab180fa2fdaff4bb4c0e9ac13825fb9ca55099f
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-04-23 23:40:06 +0300

    Learn UnitTest

---
+++ Unit Tests/Example.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Example
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ Unit Tests/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    private $calculator;
+
+    protected function setUp()
+    {
+        $this->calculator = new Example;
+    }
+
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
+    }
+
+    public function testAdd()
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
+
+}
