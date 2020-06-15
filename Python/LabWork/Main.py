b8f7cda4bbd5dd7b432fc677112f79128b0989d6
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-06-16 01:01:43 +0300

    Add complate 2/6 methods

--- Python/LabWork/Main.py
+++ Python/LabWork/Main.py
@@ -21,66 +21,53 @@ def find_max_different_price(auctions):
 with open('data.txt', 'r') as file:
     file_input = file.readlines()
 
-data_file = [string.split(',') for string in file_input]
+data_file = [string.replace(' ', '').split(',') for string in file_input]
 
 product = None
 customers = list()
 sellers = list()
+auctions = list()
 
 for data in data_file:
     for value in data:
         if value == 'product':
             product = Product(data[1], data[2], data[3], data[4], data[5])
-            print(product)
-        elif value == 'seller':
-            sellers.append(data[1], data[2], product)
         elif value == 'customer':
-            customers.append(data[1], data[2], product)
-        else
+            customers.append(Customer(data[1], data[2], product))
+        elif value == 'seller':
+            sellers.append(Seller(data[1], data[2], product))
+        elif value == 'auction':
+            auctions.append(Auction(data[1], data[2], data[3], data[4], customers, sellers))
+            sellers = list()
+            customers = list()
+        else:
             continue
-
-
-# TODO: тут будет цикл (получить все предметы из файла и насоздавать объектов)
-product = Product(2, "Test", "kek", 100, 200)
-
-customer = Customer("Alex Test Testivch", "60 1934", product)
-seller = Seller("Ivan Ivanivich Ivnov", "77 7777", product)
-
-auctions = list()
-
-for i in range(1):
-    auctions.append(Auction("Yarmarka", "Moscow", "22.05.2020", 1, customer, seller))
-    auctions.append(Auction("May", "Moscow", "22.05.2020", 1, customer, seller))
-    auctions.append(Auction("Testa", "Moscow", "22.05.2021", 2, customer, seller))
-
-
-
-
-##################################################
-print("Введите название аукциона и его дату")
-#name_auction = input()
-#date_auction = input()
-
+"""
+for auction in auctions:
+    print(auction)
+    for seller in auction.sellers:
+        print(seller)
+    for customer in auction.customers:
+        print(customer)
+"""
 
 name_auction = 'Yarmarka'
 date_auction = '22.05.2020'
-
-option_product = 'kek'
+option_product = 'Image1'
 
 for auction in auctions:
     #  Какие предметы на заданную дату и на заданном аукционе выставлены на продажу.
     if auction.name == name_auction and auction.date == date_auction:
         print(auction)
-        print(name_auction)
 
     #  На каком аукционе продавались предметы заданной специфики.
-    if auction.sellers.product.option == option_product:
-        print(auction)
-        print(option_product)
+    for seller in auction.sellers:
+        if seller.product.option == option_product:
+            print(auction)
 
-
+# TODO: написать метод для красивого вывода
 #  Предмет, имеющий максимальную разницу между начальной и конечной ценами.
-print('\nМаксимальная  между начальной и конечной ценами:\n', find_max_different_price(auctions))
+#print('\nМаксимальная  между начальной и конечной ценами:\n', find_max_different_price(auctions))
 
 #  На каком аукционе было продано больше всего предметов.
-print('\nАукцион:')
+#print('\nАукцион:')
