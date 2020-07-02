073967dda461dbb52da4226f54ce69f42174730e
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-07-02 23:41:10 +0300

    Add Hibernate

---
+++ Магистратура/1 курс 2 семестр/Практика МИН-11/project/src/main/java/com/practice/project/ObjectDAO.java
@@ -0,0 +1,155 @@
+package com.practice.project;
+
+import org.hibernate.classic.Session;
+import javax.swing.*;
+import java.util.List;
+
+public class ObjectDAO
+{
+    public void addValue(Object object)
+    {
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(object);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void updateValue(Object object)
+    {
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(object);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void deleteValue(Object object)
+    {
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(object);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public Chair getChairById(Long idObject)
+    {
+        Session session = null;
+        Chair chair = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            chair = (Chair) session.get(Chair.class, idObject);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+
+        return chair;
+    }
+
+    public Desk getDeskById(Long idObject)
+    {
+        Session session = null;
+        Desk desk = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            desk = (Desk) session.get(Desk.class, idObject);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+
+        return desk;
+    }
+
+    public Office getOfficeById(Long idObject)
+    {
+        Session session = null;
+        Office office = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            office = (Office) session.get(Office.class, idObject);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+
+        return office;
+    }
+
+    public List<Office> getAllOffice() {
+        Session session = null;
+        List<Office> offices = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.getTransaction().begin();
+            offices = (List<Office>)  HibernateUtil.getSessionFactory().openSession().createQuery("From Office").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                    "Error I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return offices;
+    }
+
+    public List<Office> hqlRequest() {
+        return (List<Office>)  HibernateUtil.getSessionFactory().openSession().createQuery("SELECT office From Chair, Desk, Office office WHERE office.name = 'Mini'").list();
+    }
+}
