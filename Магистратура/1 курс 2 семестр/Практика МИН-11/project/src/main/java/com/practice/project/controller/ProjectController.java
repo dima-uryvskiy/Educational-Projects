d6f67253bf9c7a0e5646d185e63c57500e790d72
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-07-17 21:23:34 +0300

    Final Version

--- Магистратура/1 курс 2 семестр/Практика МИН-11/project/src/main/java/com/practice/project/controller/ProjectController.java
+++ Магистратура/1 курс 2 семестр/Практика МИН-11/project/src/main/java/com/practice/project/controller/ProjectController.java
@@ -92,33 +92,4 @@ public class ProjectController {
 
         return "result";
     }
-
-//    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.GET)
-//    public String showAddPersonPage(Model model) {
-//
-//        PersonForm personForm = new PersonForm();
-//        model.addAttribute("personForm", personForm);
-//
-//        return "addPerson";
-//    }
-//
-//    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.POST)
-//    public String savePerson(Model model, //
-//                             @ModelAttribute("personForm") PersonForm personForm) {
-//
-//        String firstName = personForm.getFirstName();
-//        String lastName = personForm.getLastName();
-//
-//        if (firstName != null && firstName.length() > 0 //
-//                && lastName != null && lastName.length() > 0) {
-//            Person newPerson = new Person(firstName, lastName);
-//            persons.add(newPerson);
-//
-//            return "redirect:/personList";
-//        }
-//
-//        model.addAttribute("errorMessage", errorMessage);
-//        return "addPerson";
-//    }
-
 }
