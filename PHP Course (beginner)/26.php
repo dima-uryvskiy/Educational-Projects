fd2b04485e708d2928322667e58690b0d088415b
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-08-01 02:00:12 +0300

    Add tasks26-30

---
+++ PHP Course (beginner)/26.php
@@ -0,0 +1,10 @@
+<?php
+
+$king = 'King Balon the 6th';
+
+// BEGIN
+$numberOfCastles = 6;
+$roomsPerCastle = 17;
+
+print_r($king . ' has ' . ($numberOfCastles * $roomsPerCastle) . ' rooms.');
+// END
---
+++ PHP Course (beginner)/27.php
@@ -0,0 +1,7 @@
+<?php
+
+// BEGIN
+const DRAGONS_BORN_COUNT = 3;
+
+print_r(DRAGONS_BORN_COUNT);
+// END
