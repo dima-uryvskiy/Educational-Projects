a43fe3f114cdace2de6a02566968f8a2d1949d64
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-08-17 23:58:11 +0300

    Add tasks66*

--- PHP Course (beginner)/66.php
+++ PHP Course (beginner)/66.php
@@ -16,3 +16,25 @@ function getEvenNumbersUpTo($num)
     return $result;
 }
 // END
+
+
+// <?php
+
+// // BEGIN
+// function getEvenNumbersUpTo($num)
+// {
+//     $i = 2;
+//     $result = '';
+
+//     while ($i <= $num) {
+//         $result .= $i . ',';
+//         $i += 2;
+//     } 
+
+//     $result[-1] = '.';
+
+//     return $result;
+// }
+// // END
+
+// print_r(getEvenNumbersUpTo(9));
